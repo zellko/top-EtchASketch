093cb4cdcbdd97d3909b9bed8a153fd6dc740939
zellko <wyttenda@tutanota.com>
2022-03-09 21:40:01 +0100

    Added: Random Cell color / Correction: Cell size bug
    Added:
    - RGB button: When toggled, the cell are filled with a random color
    - Button toggle animation to show if "Black" or "RGB" is selected
    
    Correction:
    - Cells were not perfectly square --> Now using "aspect-ratio: 1;" in the CSS instead of "padding-bottom: x%".

--- js/main.js
+++ js/main.js
@@ -9,10 +9,8 @@ function createGrid(rowSize) {
     for (let i = 0; i < rowSize ** 2; ++i) {
         const grid_cell = document.createElement("div"); // Create a div
         grid_cell.classList.add("cell"); // Assign the class "cell" to the new div
-        grid_cell.style.cssText = `flex: 0 1 ${100/rowSize}%; 
-        padding-bottom: ${100/rowSize}%;`;
+        grid_cell.style.cssText = `flex: 0 1 ${100/rowSize}%;`;
         // Set the flex-basis to 100/row size. Make the cells wrap correctly
-        // Set the padding-bottom to 100/row size. Make the cells square 
         grid.appendChild(grid_cell);
     };
 };
@@ -24,13 +22,33 @@ createGrid(16);
 // *****************
 
 // Set cells color: Black or RGB
-// const btnBlack = document.querySelector("#btn-black");
-// const btnRGB = document.querySelector("#btn-rgb");
+const btnBlack = document.querySelector("#btn-black");
+const btnRGB = document.querySelector("#btn-rgb");
+let cellColor = "blackCell";
+btnBlack.classList.add("btnActive");
+
+btnBlack.addEventListener("click", function() {
+    btnBlack.classList.toggle("btnActive");
+    btnRGB.classList.toggle("btnActive");
+    cellColor = "blackCell";
+});
+
+btnRGB.addEventListener("click", function() {
+    btnBlack.classList.toggle("btnActive");
+    btnRGB.classList.toggle("btnActive");
+    cellColor = "rgbCell";
+});
 
 // Modify cells colors
 function colorCell(e) {
     if (e.buttons || e.pointerId) {
-        this.classList.add("blackCell");
+        // this.classList.add(`${cellColor}`);
+        const r = Math.floor(Math.random() * 255)
+        const g = Math.floor(Math.random() * 255)
+        const b = Math.floor(Math.random() * 255)
+        if (cellColor === "blackCell") this.style.backgroundColor = "black";
+        if (cellColor === "rgbCell") this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+        "rgb(255, 4, 4)";
     };
 }
 
@@ -47,7 +65,12 @@ const btnClear = document.querySelector("#btn-clear");
 
 function clearGrid() {
     console.log("clear");
-    cells.forEach(cell => cell.classList.remove("blackCell"));
+    cells.forEach(cell => cell.style.backgroundColor = "white");
 }
 
 btnClear.addEventListener("click", clearGrid);
+
+
+// *****************
+// Footer date
+// *****************
